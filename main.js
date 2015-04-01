ea4bf9042e88c783dfa8d5bf64bca3772d821b56
Manuel Valls Fernández <manolo@vlrz.es>
2015-04-01 10:21:31 +0200

    Su moved to 'u-su'

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-module.exports = require('u-rand').Su;
+var rand = require('u-rand');
+
+if(global.Symbol) module.exports = function(){ return Symbol(); };
+else module.exports = function(){ return rand.unique(); };
+
+module.exports.define = function(obj,su,value){
+  Object.defineProperty(obj,su,{
+    value: value,
+    writable: true,
+    configurable: true
+  });
+};
+
